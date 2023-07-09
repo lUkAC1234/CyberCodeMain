a01a2f1d64547dd003c4307d37868f14ba54119d
lUkAC2 <113376248+lUkAC1234@users.noreply.github.com>
2023-07-10 02:37:12 +0500

    'contact-us-75-faq-added'

---
+++ assets/main/scripts/js/faq.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".faq-toggle-btn");
+const faq = document.querySelectorAll(".faq");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () =>
+        button.parentElement.classList.toggle("active")
+    );
+});
