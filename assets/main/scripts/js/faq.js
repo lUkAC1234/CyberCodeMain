f718959b5fd0dd5efe233cdfa28e605ea18acba2
https://github.com/ghost160111 <jalol100.ghost@yandex.com>
2023-07-10 20:51:09 +0500

    .

--- assets/main/scripts/js/faq.js
+++ assets/main/scripts/js/faq.js
@@ -1,5 +1,8 @@
 const buttons = document.querySelectorAll(".faq-toggle-btn");
-const faq = document.querySelectorAll(".faq");
-buttons.addEventListener("click", function () {
-    faq.classList.toggle("active");
-});
+const faq     = document.querySelectorAll(".faq");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>
+        button.parentElement.classList.toggle('active')
+    )
+})
