59ac4f4429002ef16aa8a492fc3ff51232e265f1
lUkAC2 <113376248+lUkAC1234@users.noreply.github.com>
2023-10-04 19:33:21 +0500

    ''

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'webordermain',
-        'USER': 'root',
+        'USER': 'postgres',
         'PASSWORD': 'dimatop12',
         'HOST': 'localhost',
         'PORT': '5432'
